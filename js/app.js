931f55cd835a49863c51fce7197cc6849e4a3051
yechua-silva <yechua_silva@outlook.cl>
2022-12-16 17:56:19 -0300

    Agrege objeto que toma los select para filtrar

--- js/app.js
+++ js/app.js
@@ -1,11 +1,27 @@
 //VARIABLES
-const resultado = document.querySelector('#resultado');
+const marca = document.querySelector('#marca');
 const year = document.querySelector('#year');
-
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+//contenedor para los resultados
+const resultado = document.querySelector('#resultado');
 //año en el que estamos
 const max = new Date().getFullYear() ;
+//año minimo de busqueda
 const min = 2010;
-
+//generar un objetos con los filtros de busqueda
+const datosBusqueda = {
+    marca: '',
+    year: '',
+    minimo: '',
+    maximo: '',
+    puertas: '',
+    transmision: '',
+    color: ''
+}
 
 
 //FUNCIONES
@@ -45,3 +61,11 @@ document.addEventListener("DOMContentLoaded", () => {
     //LLena las opciones de años
     llenarSelect()
 });
+//eventos para los select de busqueda
+marca.addEventListener("change", e => datosBusqueda.marca = e.target.value)
+year.addEventListener("change", e => datosBusqueda.year = e.target.value)
+minimo.addEventListener("change", e => datosBusqueda.minimo = e.target.value)
+maximo.addEventListener("change", e => datosBusqueda.maximo = e.target.value)
+puertas.addEventListener("change", e => datosBusqueda.puertas = e.target.value)
+transmision.addEventListener("change", e => datosBusqueda.transmision = e.target.value)
+color.addEventListener("change", e => datosBusqueda.color = e.target.value)
